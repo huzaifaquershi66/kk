37e2964ae32facaea641591ed1508a04da6106c7
huzaifa56jbh <hadrish153@gmail.com>
2024-10-29 06:21:33 +0500

    d

--- firebase.js
+++ firebase.js
@@ -4,15 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBO8FxssGyR7jCYbj2z9R65P163fkV8T8s",
+  authDomain: "awesome-407aa.firebaseapp.com",
+  projectId: "awesome-407aa",
+  storageBucket: "awesome-407aa.appspot.com",
+  messagingSenderId: "540923471998",
+  appId: "1:540923471998:web:d6f475dccccc8cd6b8d02f",
+  measurementId: "G-DBEPJ0X9R9"
 };
 
 // Initialize Firebase
